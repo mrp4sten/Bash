2a59c25dc1c777a1ca7db7c954449fbb30abbc54
mrp4sten <mauricio.pasten.martinez@gmail.com>
2023-05-25 21:43:32 -0600

    Arrays

---
+++ 8_Arrays.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# author: mrp4sten
+
+# Declare an array of fruits
+fruits=("apple" "banana" "orange" "lemon")
+
+# Access and print individual elements
+echo "First fruit: ${fruits[0]}"
+echo "Second fruit: ${fruits[1]}"
+echo "Third fruit: ${fruits[2]}"
+echo "Fourth fruit: ${fruits[3]}"
