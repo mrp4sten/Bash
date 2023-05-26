58f51dc40c06cac9a205c305edfbdd0681135911
mrp4sten <mauricio.pasten.martinez@gmail.com>
2023-05-25 21:55:14 -0600

    Using while, break and continue

--- 7_case_statement.sh
+++ 7_case_statement.sh
@@ -2,17 +2,27 @@
 # author: mrp4sten
 read -p "Enter a fruit: " fruit
 
-case $fruit in
-apple)
-  echo "You chose an apple."
-  ;;
-banana)
-  echo "You chose a banana."
-  ;;
-orange | lemon)
-  echo "You chose an orange or a lemon."
-  ;;
-*)
-  echo "You chose an unknown fruit."
-  ;;
-esac
+#!/bin/bash
+
+while true; do
+  read -p "Enter a fruit: " fruit
+
+  case $fruit in
+  apple)
+    echo "You chose an apple."
+    break
+    ;;
+  banana)
+    echo "You chose a banana."
+    break
+    ;;
+  orange | lemon)
+    echo "You chose an orange or a lemon."
+    continue
+    ;;
+  *)
+    echo "You chose an unknown fruit."
+    continue
+    ;;
+  esac
+done
